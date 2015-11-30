577aa3b84a92c8b91eb91334585dadbb182239df
esqu1 <brandbest1@gmail.com>
2015-11-30 09:58:33 -0500

    gift1

---
+++ training/gift1/gift1.java
@@ -0,0 +1,16 @@
+/*
+ID: brandbe1
+LANG: JAVA
+TASK: gift1
+*/
+import java.util.*;
+import java.io.*;
+class gift1{
+
+
+    public static void main(String[] args) {
+	BufferedReader br = new BufferedReader(new FileReader(""));
+	PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("")));
+	StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
